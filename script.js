da678b47da3cce16545cbfab3429281f688c58b3
Nazar <wolfter12@gmail.com>
2020-11-06 20:13:12 +0200

    added period formatting with comas

--- script.js
+++ script.js
@@ -57,12 +57,18 @@ class Calculator {
   }
 
   updateOutput() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
     // console.log(`this.previousOperand = ${this.previousOperand}`);
     // console.log(`this.currentOperand = ${this.currentOperand}`);
     if (this.operation !== null && this.operation !== undefined) {
-      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
     }
+  }
+
+  getDisplayNumber(number) {
+    const floatNumber = parseFloat(number);
+    if (isNaN(floatNumber)) return '';
+    return floatNumber.toLocaleString('en');
   }
 }
 
